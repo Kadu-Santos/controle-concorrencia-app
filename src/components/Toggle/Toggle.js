d948ab56563e89933ca6c1d22ffac194b17b4151
Kadu-Santos <khadusantosed@gmail.com>
2025-11-19 20:55:51 -0300

    Correção de valores

--- src/components/Toggle/Toggle.js
+++ src/components/Toggle/Toggle.js
@@ -5,24 +5,25 @@ function Toggle({ valor, onChange }) {
   return (
     <div className="variaveis-toggle">
       <div className="radio-group">
+
         <label>
           <input
             type="radio"
-            value="sim"
-            checked={valor === true}
+            checked={valor}
             onChange={() => onChange(true)}
           />
           Sim
         </label>
+
         <label>
           <input
             type="radio"
-            value="nao"
-            checked={valor === false}
+            checked={!valor}
             onChange={() => onChange(false)}
           />
           Não
         </label>
+
       </div>
     </div>
   );
